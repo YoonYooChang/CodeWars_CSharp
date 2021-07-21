1c1b491564d0228b5e8dabd55a65d4376d8fbd65
YoonYooChang <jell0202y@gmail.com>
2021-07-21 16:58:39 +0900

    CountingDuplicates first commit

---
+++ CodeWars_Test/6_Counting Duplicates_Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, Unit2.DuplicateCount(""));
+            Assert.AreEqual(0, Unit2.DuplicateCount("abcde"));
+            Assert.AreEqual(2, Unit2.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, Unit2.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, Unit2.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, Unit2.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+    }
+}
