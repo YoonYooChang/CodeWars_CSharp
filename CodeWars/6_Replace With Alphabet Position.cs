8d962064ad1e266fee42265929dfb7cfe49f50f3
YoonYooChang <jell0202y@gmail.com>
2021-07-22 17:56:29 +0900

    ReplaceWithAlphabetPosition first commit

---
+++ CodeWars/6_Replace With Alphabet Position.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class Unit6
+    {
+        public static string AlphabetPosition(string text)
+        {
+            List<char> alphabets = text.ToLower().ToList().FindAll(c => c - 'a' >= 0 && c - 'a' <= 25);
+
+            return string.Join(" ", alphabets.Select(c => c - 'a' + 1).ToList());
+        }
+    }
+}
