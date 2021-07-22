420eb880eb4e954ef753e5992d1d343a115b8f2f
YoonYooChang <jell0202y@gmail.com>
2021-07-22 16:34:36 +0900

    Sum of Numbers first commit

---
+++ CodeWars/7_Sum of Numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class Unit4
+    {
+        public static int GetSum(int a, int b)
+        {
+            int min = a > b ? b : a;
+            int max = a > b ? a : b;
+            int sum = 0;
+
+            for (int i = min; i <= max; i++)
+                sum += i;
+
+            return sum;
+        }
+    }
+}
---
+++ CodeWars_Test/7_Sum of Numbers_Test.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, Unit4.GetSum(0, 1));
+            Assert.AreEqual(-1, Unit4.GetSum(0, -1));
+            Assert.AreEqual(2, Unit4.GetSum(-1, 2));
+            Assert.AreEqual(1, Unit4.GetSum(1, 1));
+        }
+    }
+}
