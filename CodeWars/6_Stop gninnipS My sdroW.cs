1709f7a923fe12f91df03b8975c6591fef4b6ac3
YoonYooChang <jell0202y@gmail.com>
2021-07-22 16:55:02 +0900

    Stop gninnipS My sdroW first commit

---
+++ CodeWars/6_Stop gninnipS My sdroW.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public static class Unit5
+    {
+        public static string SpinWords(string sentence)
+        {
+            string reversSentence = string.Empty;
+
+            foreach (var word in sentence.Split(" "))
+            {
+                if (word.Length >= 5)
+                {
+                    char[] charArray = word.ToCharArray();
+                    Array.Reverse(charArray);
+                    reversSentence += new string(charArray);
+                }
+                else
+                {
+                    reversSentence += word;
+                }
+                reversSentence += " ";
+            }
+            
+            return reversSentence.Trim();
+        }
+    }
+}
---
+++ CodeWars_Test/6_Stop gninnipS My sdroW_Test.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest5
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual("emocleW", Unit5.SpinWords("Welcome"));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual("Hey wollef sroirraw", Unit5.SpinWords("Hey fellow warriors"));
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual("This is a test", Unit5.SpinWords("This is a test"));
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            Assert.AreEqual("This is rehtona test", Unit5.SpinWords("This is another test"));
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            Assert.AreEqual("You are tsomla to the last test", Unit5.SpinWords("You are almost to the last test"));
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+            Assert.AreEqual("Just gniddik ereht is llits one more", Unit5.SpinWords("Just kidding there is still one more"));
+        }
+    }
+}
