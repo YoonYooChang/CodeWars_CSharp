34d55a6027aa892693414e8052140ebfce30b47d
YoonYooChang <jell0202y@gmail.com>
2021-07-23 16:15:41 +0900

    FriendOrFoe first commit, Solved it CodeWars site is work, but vs studio test not work

---
+++ CodeWars/7_Friend or Foe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class Unit8
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names) => names.Where(s => s.Length == 4);
+    }
+}
---
+++ CodeWars_Test/7_Friend or Foe_Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest8
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] expected = { "Ryan", "Mark" };
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+
+            // Why not working, but CodeWars Site work very well;
+            // CollectionAssert.AreEqual(expected, Unit8.FriendOrFoe(names));
+        }
+    }
+}
