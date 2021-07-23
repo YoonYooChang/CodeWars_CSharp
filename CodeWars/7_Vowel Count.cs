1fa6aa5c9214ab35e7a11b791eaaa2f680211d3c
YoonYooChang <jell0202y@gmail.com>
2021-07-23 13:42:58 +0900

    Vowel Count first commit

---
+++ CodeWars/7_Vowel Count.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class Unit7
+    {
+        public static int GetVowelCount(string str)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            return vowels.Select(c => str.Count(ch => ch == c)).Sum();
+        }
+    }
+}
---
+++ CodeWars_Test/7_Vowel Count_Test.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest7
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5, Unit7.GetVowelCount("abracadabra"), "Nope!");
+        }
+    }
+}
