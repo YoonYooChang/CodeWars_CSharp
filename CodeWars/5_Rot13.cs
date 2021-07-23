b954f4c3d2ccf2550cfc00045fc7936bf46bcb62
YoonYooChang <jell0202y@gmail.com>
2021-07-23 18:08:49 +0900

    Rot13 first commit

---
+++ CodeWars/5_Rot13.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace CodeWars
+{
+    public static class Unit9
+    {
+        public static string Rot13(string message) => new string(message.Select(c => char.IsLetter(c)? (char)(c + 13) > (char.IsLower(c) ? 'z' : 'Z') ? (char)(c + 13 - 26) : (char)(c + 13) : c).ToArray());
+    }
+}
---
+++ CodeWars_Test/5_Rot13_Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest9
+    {
+        [TestMethod, Description("test")]
+        public void testTest()
+        {
+            Assert.AreEqual("grfg", Unit9.Rot13("test"), String.Format("Input: test, Expected Output: grfg, Actual Output: {0}", Unit9.Rot13("test")));
+        }
+
+        [TestMethod, Description("Test")]
+        public void TestTest()
+        {
+            Assert.AreEqual("Grfg", Unit9.Rot13("Test"), String.Format("Input: Test, Expected Output: Grfg, Actual Output: {0}", Unit9.Rot13("Test")));
+        }
+    }
+}
