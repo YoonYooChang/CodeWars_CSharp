e2abb0297a4ac84553d953af3c57064bc9024306
YoonYooChang <jell0202y@gmail.com>
2021-07-26 17:25:40 +0900

    Persistent Bugger first commit

---
+++ CodeWars/6_Persistent Bugger.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace CodeWars
+{
+    public static class Unit12
+    {
+        public static int Persistence(long n)
+        {
+            int count = 0;
+
+            while (n >= 10)
+            {
+                int[] digits = n.ToString().ToCharArray().Select(c => c - '0').ToArray();
+                int value = 1;
+
+                foreach (int digit in digits)
+                {
+                    value *= digit;
+                }
+
+                n = value;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ CodeWars_Test/6_Persistent Bugger_Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest12
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("****** Basic Tests");
+            Assert.AreEqual(3, Unit12.Persistence(39));
+            Assert.AreEqual(0, Unit12.Persistence(4));
+            Assert.AreEqual(2, Unit12.Persistence(25));
+            Assert.AreEqual(4, Unit12.Persistence(999));
+        }
+    }
+}
