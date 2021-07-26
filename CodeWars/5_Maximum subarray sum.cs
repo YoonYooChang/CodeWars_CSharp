a5d955d7ec39386f9770a208ebb6656771e8bcc2
YoonYooChang <jell0202y@gmail.com>
2021-07-26 12:18:20 +0900

    MaximumSubarraySum first commit

---
+++ CodeWars/5_Maximum subarray sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public static class Unit10
+    {
+        public static int MaxSequence(int[] arr)
+        {
+            int max = int.MinValue;
+            int sequenceSum = 0;
+
+            if (arr.Length == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sequenceSum = Math.Max(0, sequenceSum) + arr[i];
+                max = Math.Max(sequenceSum, max);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ CodeWars_Test/5_Maximum subarray sum_Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest10
+    {
+        [TestMethod]
+        public void Test0()
+        {
+            Assert.AreEqual(0, Unit10.MaxSequence(new int[0]));
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual(6, Unit10.MaxSequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        }
+    }
+}
