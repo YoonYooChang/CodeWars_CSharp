1a0d3a6f11b9977b2a7063782bb854be552a7989
YoonYooChang <jell0202y@gmail.com>
2021-07-26 14:10:08 +0900

    Decode the Morse code 1 first commit

---
+++ CodeWars/6_Decode the Morse code1.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeWars
+{
+    public static class Unit11
+    {
+        public static string Decode(string morseCode)
+        {
+            string decodeString = string.Empty;
+            bool isNull = false;
+
+            foreach (string s in morseCode.Trim().Split(new char[] { ' ', '\t' }))
+            {
+                if (!string.IsNullOrEmpty(s))
+                {
+                    //Only work CodeWars
+                    //decodeString += MorseCode.Get(s);
+                    isNull = false;
+                }
+                else
+                {
+                    if (!isNull) // first space skip;
+                    {
+                        isNull = true;
+                        continue;
+                    }
+
+                    decodeString += " ";
+                }
+            }
+            return decodeString;
+        }
+    }
+}
---
+++ CodeWars_Test/6_Decode the Morse code1_Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest11
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+			try
+			{
+				string input = ".... . -.--   .--- ..- -.. .";
+				string expected = "HEY JUDE";
+
+				string actual = Unit11.Decode(input);
+
+				Assert.AreEqual(expected, actual);
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+			}
+		}
+    }
+}
