0725d15b62735959130080e4969c1577c64777e5
YoonYooChang <jell0202y@gmail.com>
2021-07-27 16:40:12 +0900

    Longest Palindrome first commit

---
+++ CodeWars/6_Longest palindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class Unit14
+    {
+        public static int GetLongestPalindrome(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return 0;
+
+            int maxLength = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = i; j < str.Length; j++)
+                {
+                    if (str.Substring(i, j - i + 1).Equals(new string(str.Substring(i, j - i + 1).Reverse().ToArray())))
+                    {
+                        maxLength = maxLength > j - i + 1 ? maxLength : j - i + 1;
+                    }
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ CodeWars_Test/6_Longest palindrome_Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest14
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, Unit14.GetLongestPalindrome("   "));
+            Assert.AreEqual(5, Unit14.GetLongestPalindrome("12 21glg"));
+            Assert.AreEqual(7, Unit14.GetLongestPalindrome("abc0cba1"));
+            Assert.AreEqual(2, Unit14.GetLongestPalindrome("baa"));
+            Assert.AreEqual(2, Unit14.GetLongestPalindrome("aa"));
+            Assert.AreEqual(1, Unit14.GetLongestPalindrome("a"));
+            Assert.AreEqual(0, Unit14.GetLongestPalindrome(null));
+            Assert.AreEqual(0, Unit14.GetLongestPalindrome(""));
+        }
+    }
+}
