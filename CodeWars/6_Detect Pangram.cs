604d9bd9239d3a15d8337c5e9c4190323f82d350
YoonYooChang <jell0202y@gmail.com>
2021-07-27 12:09:44 +0900

    DetectPangram first commit

---
+++ CodeWars/6_Detect Pangram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class Unit13
+    {
+        public static bool IsPangram(string str)
+        {
+            return str.ToUpper().ToCharArray().ToList().FindAll(c => Char.IsLetter(c)).GroupBy(c => c).Count() == 26;
+        }
+    }
+}
---
+++ CodeWars_Test/6_Detect Pangram_Test.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest13
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, Unit13.IsPangram("The quick brown fox jumps over the lazy dog."));
+        }
+    }
+}
