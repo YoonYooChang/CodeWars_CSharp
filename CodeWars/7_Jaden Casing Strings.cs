7db0c6ddfb1e452340a1628105a3fc3019da0067
YoonYooChang <jell0202y@gmail.com>
2021-09-03 12:15:30 +0900

    JadenCasingStrings

---
+++ CodeWars/7_Jaden Casing Strings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class Unit17
+    {
+        public static string ToJadenCase(this string phrase) => string.Join(" ", phrase.Split(' ').Select(word => word.ToUpper().Substring(0, 1) + word.ToLower().Substring(1)));
+    }
+}
---
+++ CodeWars_Test/7_Jaden Casing Strings_Test.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars_Test
+{
+    [TestClass]
+    public class UnitTest17
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("How Can Mirrors Be Real If Our Eyes Aren't Real",
+                "How can mirrors be real if our eyes aren't real".ToJadenCase(),
+                "Strings didn't match.");
+        }
+    }
+}
